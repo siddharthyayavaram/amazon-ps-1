343385e986289918940e2724cc6cae4cb00ef2ec
siddharthyayavaram <68839837+siddharthyayavaram@users.noreply.github.com>
2023-06-29 02:45:11 +0530

    Update client.py

--- amazon-client/client.py
+++ amazon-client/client.py
@@ -1,5 +1,20 @@
 import requests
 
+"""
+test data
+        "id": 1,
+        "length": 139.0,
+        "breadth": 5.0,
+        "height": 5.0,
+        "mode": 2,
+        "slot": 1.0,
+        "days": 1.0,
+        "lat1": 37.7749,
+        "lon1": -122.4194,
+        "lat2": 38.5816,
+        "lon2": -121.4944
+"""
+
 def get_cost(id):
     url="http://127.0.0.1:8000/api/ships_list/"+str(id)+"/"
     response=requests.get(url)
@@ -18,12 +33,29 @@ def get_last_id():
 def add_data():
     url="http://127.0.0.1:8000/api/ships_list/"
 
-    time = float(input("Enter the time: "))
-    zone = input("Enter the zone: ")
-    weight = float(input("Enter the weight: "))
-    carrier = input("Enter the carrier: ")
+    length = float(input("Enter the length: "))
+    breadth = float(input("Enter the breadth: "))
+    height = float(input("Enter the height: "))
+    mode = int(input("Enter the mode: "))
+    slot = float(input("Enter the slot: "))
+    days = float(input("Enter the number of days: "))
+    lat1 = float(input("Enter the latitude of point 1: "))
+    lon1 = float(input("Enter the longitude of point 1: "))
+    lat2 = float(input("Enter the latitude of point 2: "))
+    lon2 = float(input("Enter the longitude of point 2: "))
 
-    data = {'time': time,'zone': zone,'weight': weight,'carrier': carrier}
+    data = {
+    "length": length,
+    "breadth": breadth,
+    "height": height,
+    "mode": mode,
+    "slot": slot,
+    "days": days,
+    "lat1": lat1,
+    "lon1": lon1,
+    "lat2": lat2,
+    "lon2": lon2
+}
 
     response = requests.post(url, data=data)
     response.raise_for_status()
